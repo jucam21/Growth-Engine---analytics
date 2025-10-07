d1f9e974765046e5dc0e245e62740ab53ed9da38
Juan Salgado <juan.salgado@zendesk.com>
2025-10-07 12:58:06 -0600

    Investigating null modal wins

--- Trial conversion exploration/Dashboard/tableau_dashboard.sql
+++ Trial conversion exploration/Dashboard/tableau_dashboard.sql
@@ -31,7 +31,7 @@ with bookings as (
 modal_buy_now as (
     select
         mapping.crm_account_id,
-        buy_now.account_id,
+        --buy_now.account_id,
         max(date(original_timestamp)) as max_date
     from
         cleansed.segment_support.growth_engine_trial_cta_1_buy_now_scd2 buy_now
@@ -51,8 +51,8 @@ modal_buy_now as (
 modal_see_all_plans as (
     select
         mapping.crm_account_id,
-        see_all_plans.account_id,
-        trial_accounts.win_date,
+        --see_all_plans.account_id,
+        --trial_accounts.win_date,
         max(date(original_timestamp)) as max_date
     from
         cleansed.segment_support.growth_engine_trial_cta_1_see_all_plans_scd2 see_all_plans
@@ -86,12 +86,13 @@ billing_cart_loaded as (
         on 
             trial_accounts.instance_account_id = billing_cart_loaded.account_id
             and date(billing_cart_loaded.original_timestamp) <= trial_accounts.win_date
-            and date(billing_cart_loaded.original_timestamp) >= dateadd('day', -15, trial_accounts.win_date)
+            -- Last ever billing cart loaded event before win date
+            --and date(billing_cart_loaded.original_timestamp) >= dateadd('day', -15, trial_accounts.win_date)
     --- CRM
     left join foundational.customer.entity_mapping_daily_snapshot mapping
         on 
             billing_cart_loaded.account_id = mapping.instance_account_id
-            and date(billing_cart_loaded.original_timestamp) = mapping.source_snapshot_date
+            --and date(billing_cart_loaded.original_timestamp) = mapping.source_snapshot_date
     where paid_customer = FALSE
     qualify row_number() over (partition by mapping.crm_account_id order by billing_cart_loaded.original_timestamp desc) = 1
 ),
@@ -177,7 +178,59 @@ bookings_daily as (
     where
         bookings_.pro_forma_signature_date >= '2025-05-01'
     group by all
+),
+
+bookings_daily_v2 as (
+    select 
+        bookings_.crm_account_id,
+        bookings_.pro_forma_signature_date,
+        case
+            when
+                modal_buy_now_.crm_account_id is null
+                and modal_see_all_plans_.crm_account_id is null
+                then bookings_.crm_account_id else null
+        end as no_modal,
+        case 
+            when billing_cart_loaded_.max_date is null then 'No cart event' 
+            else 'cart_event'
+        end as cart_event,
+        billing_cart_loaded_.origin
+    from bookings bookings_
+    left join modal_buy_now modal_buy_now_
+        on bookings_.crm_account_id = modal_buy_now_.crm_account_id
+    left join modal_see_all_plans modal_see_all_plans_
+        on bookings_.crm_account_id = modal_see_all_plans_.crm_account_id
+    left join billing_cart_loaded billing_cart_loaded_
+        on bookings_.crm_account_id = billing_cart_loaded_.crm_account_id
+    where
+        bookings_.pro_forma_signature_date >= '2025-05-01'
+    group by all
 )
+
+select *
+from bookings_daily_v2
+where
+    date_trunc('day', pro_forma_signature_date) = '2025-09-30'
+    and no_modal is not null
+    --and cart_event = 'No cart event'
+    and origin = 'trial_welcome_screen'
+--limit 10
+
+
+
+
+select 
+    date_trunc('day', pro_forma_signature_date) as month_,
+    count(*) tot_obs,
+    count(distinct crm_account_id) tot_crm_accounts,
+    sum(case when no_modal is not null then 1 else 0 end) tot_no_modal
+from bookings_daily_v2
+group by 1
+order by 1 desc
+limit 20
+
+
+
 
 
 select *, CONVERT_TIMEZONE('UTC', 'America/Los_Angeles', CURRENT_TIMESTAMP) as updated_at
@@ -187,42 +240,179 @@ from bookings_daily
 
 
 
+--- CRMs without modal events
+001PC00000W3pZqYAJ
+001PC00000WA32NYAT
+001PC00000W6wYLYAZ
+001PC00000W4dVAYAZ
+001PC00000W7cSqYAJ
+001PC00000VMdoyYAD
 
 
 
+select 
+    source_snapshot_date,
+    crm_account_id,
+    instance_account_id
+from foundational.customer.entity_mapping_daily_snapshot
+where crm_account_id = '001PC00000W3pZqYAJ'
+order by source_snapshot_date desc
+limit 10
 
+
+
+--- Has modal load but no clicks
+select 
+    convert_timezone('UTC', 'America/Los_Angeles', original_timestamp) as original_timestamp_pt,
+    *
+from cleansed.segment_support.growth_engine_trial_cta_1_modal_load_scd2
+where account_id = 25677258
+order by original_timestamp desc
 
 
 
 
 
 select 
-    count(*) tot_obs,
-    count(distinct account_id) account_id,
-    sum(case when crm_account_id is null then 1 else null end) no_crm_account_id
-from modal_see_all_plans
+    convert_timezone('UTC', 'America/Los_Angeles', original_timestamp) as original_timestamp_pt,
+    *
+from cleansed.segment_support.growth_engine_trial_cta_1_buy_now_scd2
+where account_id = 25677258
+order by original_timestamp desc
 
 
 
 
-select *
-from modal_see_all_plans
-where crm_account_id is null
-limit 10
+select 
+    convert_timezone('UTC', 'America/Los_Angeles', original_timestamp) as original_timestamp_pt,
+    *
+from cleansed.segment_support.growth_engine_trial_cta_1_see_all_plans_scd2
+where account_id = 25677258
+order by original_timestamp desc
 
 
 
---2025-09-26
 
 
-select *
+
+
+
+--- CRMs without cart loaded event
+
+001PC00000W86h1YAB
+001PC00000GgjdqYAB
+001PC00000W7OBMYA3
+001PC00000W9UVtYAN
+001PC00000W4wxVYAR
+001PC00000W2GHjYAN
+001PC00000W3OD6YAN
+001PC00000W3SrtYAF
+001PC00000W9ITEYA3
+001PC00000VzUKZYA3
+001PC00000HzDugYAF
+
+
+
+select 
+    source_snapshot_date,
+    crm_account_id,
+    instance_account_id
 from foundational.customer.entity_mapping_daily_snapshot
-where instance_account_id = 25757522
-order by source_snapshot_date 
+where crm_account_id = '001PC00000W86h1YAB'
+order by source_snapshot_date desc
 limit 10
 
 
 
+
+
+select 
+    convert_timezone('UTC', 'America/Los_Angeles', original_timestamp) as original_timestamp_pt,
+    date(billing_cart_loaded.original_timestamp),
+    *
+from cleansed.segment_billing.segment_billing_cart_loaded_scd2 billing_cart_loaded
+where 
+    account_id = 25782488
+    and paid_customer = FALSE
+order by original_timestamp desc
+
+
+
+select 
+    instance_account_id,
+    win_date
+from presentation.growth_analytics.trial_accounts
+where instance_account_id = 25782488
+
+
+
+select 
+    convert_timezone('UTC', 'America/Los_Angeles', original_timestamp) as original_timestamp_pt,
+    *
+from cleansed.segment_support.growth_engine_trial_cta_1_modal_load_scd2
+where account_id = 25782488
+order by original_timestamp desc
+
+
+
+
+
+
+
+--- CRMs wiout modal event & trial_welcome_screen origin
+
+001PC00000W53dxYAB
+001PC00000VyvBNYAZ
+001PC00000W71mhYAB
+001PC00000VyQ5xYAF
+001PC00000OlI8bYAF
+001PC00000VynwvYAB
+001PC00000W4V9OYAV
+001PC00000W1hWlYAJ
+001PC00000W4dVAYAZ
+001PC00000VypcEYAR
+001PC00000W5H1CYAV
+001PC00000W6LdpYAF
+0011E00001kVZFEQA4
+001PC00000W2YmNYAV
+
+
+
+select 
+    source_snapshot_date,
+    crm_account_id,
+    instance_account_id
+from foundational.customer.entity_mapping_daily_snapshot
+where crm_account_id = '001PC00000W53dxYAB'
+order by source_snapshot_date desc
+limit 10
+
+
+select 
+    convert_timezone('UTC', 'America/Los_Angeles', original_timestamp) as original_timestamp_pt,
+    *
+from cleansed.segment_support.growth_engine_trial_cta_1_modal_load_scd2
+where account_id = 25773298
+order by original_timestamp desc
+
+
+select *
+    --pro_forma_signature_date,
+    --crm_account_id,
+    --crm_opportunity_id,
+    --total_booking_arr_usd
+    ----sum(total_booking_arr_usd) as total_booking_arr_usd
+from functional.finance.sfa_crm_bookings_current
+where crm_account_id = '001PC00000W53dxYAB'
+
+
+select
+    instance_account_id,
+    instance_account_created_date,
+    win_date,
+    sales_model_at_win
+from presentation.growth_analytics.trial_accounts
+where instance_account_id = 25773298
 
 
 
